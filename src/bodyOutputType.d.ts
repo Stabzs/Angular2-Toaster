f137924f8b4269366625e96c620a40a52fc7c259
Stabzs <davecenter2002@gmail.com>
2016-02-15 15:26:49 -0700

    Removed all src files other than toaster.css and ts files

--- src/bodyOutputType.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum BodyOutputType {
-    Default = 0,
-    TrustedHtml = 1,
-    Component = 2,
-}
--- src/bodyOutputType.js
+++
@@ -1,7 +0,0 @@
-(function (BodyOutputType) {
-    BodyOutputType[BodyOutputType["Default"] = 0] = "Default";
-    BodyOutputType[BodyOutputType["TrustedHtml"] = 1] = "TrustedHtml";
-    BodyOutputType[BodyOutputType["Component"] = 2] = "Component";
-})(exports.BodyOutputType || (exports.BodyOutputType = {}));
-var BodyOutputType = exports.BodyOutputType;
-//# sourceMappingURL=bodyOutputType.js.map
